5ef1186b32cdf03f39a5c633443a26c4d67e5a66
Marius Oscar Moe <mariuom@stud.ntnu.no>
2015-12-01 12:47:01 +0100

    fix all

--- Hobby/krakopass_v1.py
+++ Hobby/krakopass_v1.py
@@ -1,97 +1,104 @@
-
-
 import random
-import datetime
+
 #from datetime import date 
 #from PIL import ImageTk, Image
 #from Tkinter import *
 import os
+import time
 import magic
 import wand
-import Tkinter
+import datetime
+
+import datetime
 import pygame
 import pygame.camera
+import time
 
-from wand.image import Image as imo
 
-brightnes = 100
+
+from wand.display import display
+from wand.drawing import Drawing
+
+from wand.image import Image
+#import wand.image as darr
+
+brightnes = 64
 imgName = ""
 
+class takeImage:
+	'takes image with webcam'
+	def __init__(self, name):		#name	name of image
+		self.name = name
+	#IMPORTANT https://www.pygame.org/docs/ref/camera.html
+	def get_picture(self):
+		pygame.camera.init()
+		print(pygame.camera.list_cameras())
+		#pygame.camera.list_camera() #Camera detected or not
+		cam = pygame.camera.Camera("/dev/video0",(640,480), "RGB")		#video0 for local webcam and video1 for external
+		cam.start()
+		print(cam.get_controls())
+		cam.set_controls(False, False, 200)
+		print(cam.get_controls())
+		camImg = cam.get_image()
+		 
+		print(imgName)
+		
+		#------------------------------
+		pygame.image.save(camImg,str(self.name))
+		#------------------------------
+		
+		cam.stop()
+		pygame.camera.quit()
+		pygame.quit()
+		
+	
 
-#IMPORTANT https://www.pygame.org/docs/ref/camera.html
-def get_picture():
-	pygame.camera.init()
-	print(pygame.camera.list_cameras())
-	#pygame.camera.list_camera() #Camera detected or not
-	cam = pygame.camera.Camera("/dev/video0",(640,480))		#video0 for local webcam and video1 for external 
-	cam.start()
-	print(cam.get_controls())
-	cam.set_controls(False, False, brightnes)
-	print(cam.get_controls())
-	img = cam.get_image()
-	imgName = str(datetime.datetime.now())+ ".jpg" 
-	#print(imgName)
 	
-	#------------------------------
-	pygame.image.save(img,str(imgName))
-	#------------------------------
-	drawOnImage()
-	
+
+
+
 #draws some stuff on the picture so that it looks like a passport
 def drawOnImage():
+	navn = raw_input("Navn: ")
+	farge = raw_input("Farge: ")
+	patrulje = raw_input("patrulje: ")
 	
-	with imo(filename=imgName) as i:
-		i.type = 'grayscale';
-		i.save(filename=imgName);
+	navn = "Navn: " + str(navn)
+	farge = 'farge: ' + str(farge)
+	patrulje = 'patrulje: ' + str(patrulje)
 	
-	#m=magic.open(magic.MAGIC_NONE)
-	#m.load()
+	imgName = (str(datetime.datetime.now())).replace(" ", "") + ".png"
+	take1 = takeImage(imgName)
+	take1.get_picture()
+	time.sleep(2)
+	file_path = imgName
 	
-	#Draw on the image so it looks like a passport
-	placeForPrint()
 
-#prints or moves the passport	
-def placeForPrint():
-	#print the passport
-	#else put the picture in a folder named pasports
-	pass
+	print("---grayscale image---")
+	#var1 = raw_input("enter something: ")
+	with Image(filename=file_path) as img:
+		print(img.size)
+		img.type = 'grayscale';
+		with Image(filename='watermark.png') as fg_img:
+			img.composite(fg_img, left=100, top=300)
+			with Drawing() as draw:
+				#add some text here
+				draw.font_size = 30
+				draw.text(100, 100, navn)
+				draw.text(100, 250, farge)
+				draw.text(100, 200, patrulje)
+				with Image(filename='hvit.png') as hvit_img:
+						draw(hvit_img)
+						with Image(width=img.width+hvit_img.width, height=img.height) as big_img:
+							big_img.composite_channel('default_channels',img,'over', 0, 0)
+							big_img.composite_channel('default_channels',hvit_img,'blend', hvit_img.width, 0)
+							big_img.resize(big_img.width/2, big_img.height/2)
+							big_img.save(filename=file_path);
+							print(big_img.size)
 
 
 
-
-
-
-def init():
-	get_picture()
-#init()
+drawOnImage()
 	
-	
-from Tkinter import *
 
-class Application(Frame):
 
-    def createWidgets(self):
-        self.QUIT = Button(self)
-        self.QUIT["text"] = "QUIT"
-        self.QUIT["fg"]   = "red"
-        self.QUIT["command"] =  self.quit
-
-        self.QUIT.pack({"side": "left"})
-
-        self.create = Button(self)
-        self.create["text"] = "Create Pass",
-        self.create["command"] = init()
-
-        self.create.pack({"side": "left"})
-
-    def __init__(self, master=None):
-        Frame.__init__(self, master)
-        self.pack()
-        self.createWidgets()
-
-root = Tk()
-root.geometry('{}x{}'.format(600, 200))		#set the size of the window
-
-app = Application(master=root)
-app.mainloop()
-root.destroy()
